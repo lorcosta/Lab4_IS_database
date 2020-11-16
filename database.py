6f59e159a1f6e6bf96243de9f1429ef7173031e5
Lorenzo Costa <costa.lorenzo98@gmail.com>
2020-11-16 20:53:29 +0100

    Changes as suggested, errors in import

--- database.py
+++ database.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
+from database import User, Role
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'qwertyuiopasdfghjklzxcvbnm'
 from form import SignUp
+
 
 @app.before_first_request
 def setup_db():
-    user_role = Role(name='User')
-    user_susan = User(username='susan', role=user_role)
-    db.session.add_all([user_susan])
+    """user_role = Role(role='User')
+    db.session.add(User(username='susan', role=user_role))"""
+
+    user_instance = User(name='susan')
+    user_from_db = User.query.filter_by(name='lorenzo').first()
+    with app.push_context():
+        db.session.add(user_instance)
+        user_instance.role = Role(authorisation='moderator')
+        user_from_db.Role(authorisation='admin')
+        db.session.commit()
+    for role in Role.query.filter_by(authorisation='admin').all():
+        print(User.query.get(role.users.id))  # prints all the admins
+    for role in Role.query.filter_by(authorisation='moderators').all():
+        print(User.query.get(role.users.id))  # prints all the moderators
     db.create_all()
 
 
@@ -21,7 +36,7 @@ def signup():
     signUp = SignUp()
     if signUp.validate_on_submit():
         return redirect('database_content')
-    return render_template('signup.html')
+    return render_template('signup.html', signUp=signUp)
 
 
 @app.route('/content')
@@ -51,4 +66,4 @@ class Role(db.Model):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
