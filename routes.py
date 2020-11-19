4978d8e08d6210c67bd248246610b436efbc2d2a
Lorenzo Costa <costa.lorenzo98@gmail.com>
2020-11-19 16:17:05 +0100

    Changes to imports

--- routes.py
+++ routes.py
@@ -1,34 +1,33 @@
 from form import SignUp
-from run import app, db
 from flask import render_template, redirect
 from Models import User, Role
 
 
-@app.before_first_request
-def setup_db():
-    user_instance = User(name='susan')
-    user_from_db = User.query.filter_by(name='lorenzo').first()
-    with app.push_context():
-        db.session.add(user_instance)
-        user_instance.role = Role(authorisation='moderator')
-        user_from_db.Role(authorisation='admin')
-        db.session.commit()
-    for role in Role.query.filter_by(authorisation='admin').all():
-        print(User.query.get(role.users.id))  # prints all the admins
-    for role in Role.query.filter_by(authorisation='moderators').all():
-        print(User.query.get(role.users.id))  # prints all the moderators
-    db.create_all()
+def apply_routing(app):
+    @app.before_first_request
+    def setup_db():
+        user_instance = User(name='susan')
+        user_from_db = User.query.filter_by(name='lorenzo').first()
+        with app.push_context():
+            db.session.add(user_instance)
+            user_instance.role = Role(authorisation='moderator')
+            user_from_db.Role(authorisation='admin')
+            db.session.commit()
+        for role in Role.query.filter_by(authorisation='admin').all():
+            print(User.query.get(role.users.id))  # prints all the admins
+        for role in Role.query.filter_by(authorisation='moderators').all():
+            print(User.query.get(role.users.id))  # prints all the moderators
+        db.create_all()
 
+    @app.route('/signup', methods=['POST', 'GET'])
+    def signup():
+        sign_up = SignUp()
+        if sign_up.validate_on_submit():
+            return redirect('database_content')
+        return render_template('signup.html', signUp=sign_up)
 
-@app.route('/signup', methods=['POST', 'GET'])
-def signup():
-    sign_up = SignUp()
-    if sign_up.validate_on_submit():
-        return redirect('database_content')
-    return render_template('signup.html', signUp=sign_up)
-
-
-@app.route('/content')
-def database_content():
-    User.query.all()
-    return render_template('databaseContent.html')
+    @app.route('/content')
+    def database_content():
+        User.query.all()
+        return render_template('databaseContent.html')
+    return app
--- run.py
+++ run.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
+from routes import apply_routing
+app = apply_routing(app=Flask(__name__))
 db = SQLAlchemy(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SECRET_KEY'] = 'qwertyuiopasdfghjklzxcvbnm'
 
+# from routes import apply_routing
+app = apply_routing(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    apply_routing(app).run(debug=True)
