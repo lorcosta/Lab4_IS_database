5345c7bdd2bb6428b0ad0a62e6500f21722b9155
Lorenzo Costa <costa.lorenzo98@gmail.com>
2020-11-24 09:16:03 +0100

    Solved problems, implemented form and mail but problems with imports for uploading images

--- Models.py
+++ Models.py
@@ -2,23 +2,22 @@ from run import db
 
 
 class User(db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    name = db.Column(db.String(64))
-    email = db.Column(db.String(30))
-    password = db.Column(db.String(64))
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 
 class Role(db.Model):
-    __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    authorisation = db.Column(db.String(60), default='admin')
+    name = db.Column(db.String(20), nullable=False)
+    authorisation = db.Column(db.String(60), default='user', unique=True)
     users = db.relationship('User', backref='role')
 
     def __repr__(self):
-        return '<Role %r>' % self.role
+        return '<Role %r>' % self.name
